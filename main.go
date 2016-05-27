6197c7de5c7eb543d8ab71a590061a4f0d879d9c
jwdeitch <jwdeitch@gmail.com>
2016-05-26 23:43:01 -0400

    init

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/tealeg/xlsx"
+	"fmt"
+	"net/http"
+	"encoding/json"
+	. "github.com/inturn/go-helpers"
+)
+
+type Row struct {
+	Cells      []string
+	LineNumber int
+}
+
+var Rows []Row
+
+func main() {
+	fmt.Println("running: http://localhost:9090")
+
+	http.HandleFunc("/process", process)
+	err := http.ListenAndServe(":9090", nil)
+	Check(err)
+
+	fmt.Println("DONE")
+}
+
+func process(w http.ResponseWriter, r *http.Request) {
+	openXlsx("/Users/jordan1/Desktop/Untitled.xlsx")
+	json, err := json.Marshal(Rows)
+	Check(err)
+	fmt.Printf(string(json))
+	//fmt.Fprintf(w, "/process")
+
+}
+
+func openXlsx(path string) {
+	excelFileName := path
+	xlFile, err := xlsx.OpenFile(excelFileName)
+	Check(err)
+
+	for _, sheet := range xlFile.Sheets {
+		for rowNumber, row := range sheet.Rows {
+			var Cells []string
+			for _, cell := range row.Cells {
+				Cells = append(Cells,cell.Value)
+			}
+
+			Rows = append(Rows, Row{Cells,rowNumber})
+		}
+	}
+
+}
