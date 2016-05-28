91516fa19aedffe29dc9bcf02f40efbcf476a4b2
jwdeitch <jwdeitch@gmail.com>
2016-05-28 15:51:16 -0400

    asset display on front end

--- main.go
+++ main.go
@@ -17,11 +17,8 @@ type Row struct {
 	LineNumber int
 }
 
-var Rows []Row
-
 func main() {
 	fmt.Println("running: http://localhost:9090")
-	http.HandleFunc("/process", process)
 	http.HandleFunc("/recieve", receive)
 	err := http.ListenAndServe(":9090", nil)
 	Check(err)
@@ -36,31 +33,25 @@ func receive(w http.ResponseWriter, r *http.Request) {
 
 	defer file.Close()
 	time := strconv.Itoa(int(time.Now().Unix()))
-	filePath := "./uploads/"+handler.Filename + "_" + time
-	f, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0666)
+	filePath := "./uploads/" + handler.Filename + "_" + time
+	f, err := os.OpenFile(filePath, os.O_WRONLY | os.O_CREATE, 0666)
 	Check(err)
 
 	defer f.Close()
 	io.Copy(f, file)
 
-	openXlsx(filePath)
+	Rows := openXlsx(filePath)
 	json, err := json.Marshal(Rows)
 	Check(err)
 	fmt.Fprintf(w, string(json))
 
 }
 
-func process(w http.ResponseWriter, r *http.Request) {
-	openXlsx("/Users/jordan1/Desktop/Untitled.xlsx")
-	json, err := json.Marshal(Rows)
-	Check(err)
-	fmt.Fprintf(w, string(json))
-}
-
-func openXlsx(path string) {
+func openXlsx(path string) []Row {
 	xlFile, err := xlsx.OpenFile(path)
 	Check(err)
 
+	var Rows []Row
 	for _, sheet := range xlFile.Sheets {
 		for rowNumber, row := range sheet.Rows {
 			if rowNumber == 10 {
@@ -69,11 +60,12 @@ func openXlsx(path string) {
 
 			var Cells []string
 			for _, cell := range row.Cells {
-				Cells = append(Cells,cell.Value)
+				Cells = append(Cells, cell.Value)
 			}
-
-			Rows = append(Rows, Row{Cells,rowNumber})
+			Rows = append(Rows, Row{Cells, rowNumber})
 		}
 	}
+
+	return Rows
 
 }
--- templates/js/js.js
+++ templates/js/js.js
@@ -1,14 +1,13 @@
 $(document).ready(function () {
-
     // Thanks! http://stackoverflow.com/a/16404503/4603498
     var clicked = false;
     $(".uploadInput").change(function () {
         if ($(this).val() != "") {
             $('.upload').submit();
-            $('.container').addClass('spinner').html("").height(207);
+            $('.upload-step').hide();
+            $('.container').addClass('spinner').height(207);
         }
     });
-
     $(".upload").click(function () {
         if (!clicked) {
             clicked = true;
@@ -23,11 +22,18 @@ $(document).ready(function () {
             contentType: false,
             processData: false,
             data: new FormData($('.upload')[0]),
-            success: function() {
+            success: function (data) {
                 $('.container').removeClass("spinner").animate({
                     width: "80%"
+                });
+                new Vue({
+                    el: '.container',
+                    data: {
+                        "spreadsheet": JSON.parse(data)
+                    }
                 })
             }
         });
     });
+
 });
