b73044671f4e1fe98ba5c83164b9fc0a49ee6a33
jwdeitch <jwdeitch@gmail.com>
2016-05-28 09:03:09 -0400

    init

--- main.go
+++ main.go
@@ -21,17 +21,13 @@ func main() {
 	http.HandleFunc("/process", process)
 	err := http.ListenAndServe(":9090", nil)
 	Check(err)
-
-	fmt.Println("DONE")
 }
 
 func process(w http.ResponseWriter, r *http.Request) {
 	openXlsx("/Users/jordan1/Desktop/Untitled.xlsx")
 	json, err := json.Marshal(Rows)
 	Check(err)
-	fmt.Printf(string(json))
-	//fmt.Fprintf(w, "/process")
-
+	fmt.Fprintf(w, string(json))
 }
 
 func openXlsx(path string) {
@@ -41,6 +37,10 @@ func openXlsx(path string) {
 
 	for _, sheet := range xlFile.Sheets {
 		for rowNumber, row := range sheet.Rows {
+			if rowNumber == 10 {
+				break
+			}
+
 			var Cells []string
 			for _, cell := range row.Cells {
 				Cells = append(Cells,cell.Value)
